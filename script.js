cd375507cb505a1a2ea815c8414fed4b428a2405
Mathew Schaeffer <mathew.schaeffer@outlook.com>
2024-04-24 21:19:26 +1000

    add function to determine winning combinations

--- script.js
+++ script.js
@@ -6,52 +6,95 @@ By Mathew Schaeffer
 
 */
 
-const main = (() => {
-    const init = () => {
-
-        // create game board
-        const gameBoard = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
-        ];
-
-        const winningCombinations = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [1, 5, 9],
-            [3, 5, 7],
-            [1, 4, 7],
-            [2, 5, 8],
-            [3, 6, 9],
-        ]
-
-        console.log(gameBoard);
-
-        // create players
-        function createPlayer(name, score, turns) {
-            return {
-                name: name,
-                score: score,
-                turns: turns,
-            }
-        }
-
-        player1 = createPlayer("Player One", 0, []);
-        player2 = createPlayer("Player Two", 0, []);
-        player1.turns = [1, 2, 3];
-
-        // check winning combinations
-
-        for (let combinations of winningCombinations) {
-            if (combinations.includes()) {
-
-            }
-        };
+(function () {
+    init() {
+      let counter = 0;
+      let turnsTaken = 0;
+      const winCodes = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+      ];
+  
+      const test1 = [1, 2, 3];
+      const test2 = [4, 5, 6];
+      const test3 = [9, 8, 7];
+      const test4 = [5, 1, 2, 3];
+      const test5 = [9, 6, 3, 2, 1];
+      const test11 = [1, 3, 5, 7];
+      const test6 = [1, 4, 9];
+      const test7 = [1, 6, 8];
+      const test8 = [1, 2, 4, 5];
+      const test9 = [0, 1, 8, 9];
+      const test10 = [1, 3, 7, 9];
+      const test12 = [5, 8, 7];
+  
+      console.log(checkWin(test1));
+      console.log(checkWin(test2));
+      console.log(checkWin(test3));
+      console.log(checkWin(test4));
+      console.log(checkWin(test5));
+      console.log(checkWin(test11));
+      console.log("---------------");
+      console.log(checkWin(test6));
+      console.log(checkWin(test7));
+      console.log(checkWin(test8));
+      console.log(checkWin(test9));
+      console.log(checkWin(test10));
+      console.log(checkWin(test12));
     }
-
-    init();
-
-
-})();
+      
+  
+      function checkWin(arr) {
+        for (let i = 0; i < winCodes.length; i++) {
+          counter = 0;
+          for (let j = 0; j < arr.length; j++) {
+            if (winCodes[i].includes(arr[j])) {
+              counter++;
+              if (counter === 3) {
+                return true;
+              }
+            }
+          }
+        }
+        return false;
+      }
+  
+  })();
+  
+  // function checkWin(arr) {
+  //   for (let i = 0; i < winCodes.length; i++) {
+  //     counter = 0;
+  //     for (let j = 0; j < arr.length; j++) {
+  //       if (winCodes[i].includes(arr[j])) {
+  //         counter++;
+  //         if (counter === 3) {
+  //           return true;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return false;
+  // }
+  
+  // test() {
+  //   console.log(checkWin(test1));
+  // console.log(checkWin(test2));
+  // console.log(checkWin(test3));
+  // console.log(checkWin(test4));
+  // console.log(checkWin(test5));
+  // console.log(checkWin(test11));
+  // console.log("---------------");
+  // console.log(checkWin(test6));
+  // console.log(checkWin(test7));
+  // console.log(checkWin(test8));
+  // console.log(checkWin(test9));
+  // console.log(checkWin(test10));
+  // console.log(checkWin(test12));
+  // };
+  
