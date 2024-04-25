fc90ceb1972bb2408bde1c792e9dac5e3157e939
Mathew Schaeffer <mathew.schaeffer@outlook.com>
2024-04-25 15:59:02 +1000

    added prototype for game loop

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-/*
-
-Tic Tac Toe made for the Odin Project
-
-By Mathew Schaeffer
-
-*/
-
 (function game() {
 
-    const test1 = [1, 2, 3];
-    const test2 = [4, 5, 6];
-    const test3 = [9, 8, 7];
-    const test4 = [5, 1, 2, 3];
-    const test5 = [9, 6, 3, 2, 1];
-    const test11 = [1, 3, 5, 7];
-    const test6 = [1, 4, 9];
-    const test7 = [1, 6, 8];
-    const test8 = [1, 2, 4, 5];
-    const test9 = [0, 1, 8, 9];
-    const test10 = [1, 3, 7, 9];
-    const test12 = [5, 8, 7];
+    function createPlayer(name, score, turns, playerIcon) {
+        return { name, score, turns, playerIcon };
+    };
+
+    const player1 = createPlayer("Player1", 0, [], "X");
+    const player2 = createPlayer("Player2", 0, [], "Y");
 
     let counter = 0;
     let turnsTaken = 0;
+    let isGameRunning = true;
 
     const winCodes = [
         [1, 2, 3],
@@ -35,20 +22,119 @@ By Mathew Schaeffer
         [3, 5, 7]
     ];
 
+    const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     function checkWin(arr) {
+
         for (let i = 0; i < winCodes.length; i++) {
             counter = 0;
             for (let j = 0; j < arr.length; j++) {
                 if (winCodes[i].includes(arr[j])) {
                     counter++;
                     if (counter === 3) {
+                        isGameRunning = false;
+                        console.log("WINNER IS X")
+                        player1.turns = [];
+                        player2.turns = [];
+                        console.log(player1.turns);
                         return true;
                     }
                 }
             }
+
+            return false;
         }
-        return false;
+
     }
+
+    renderConsole();
+
+    // while (isGameRunning) {
+
+    turn = parseInt(prompt("Player X Turn"));
+    gameBoard.splice(turn - 1, 1, "X");
+    player1.turns.push(turn);
+    console.log(`player turns ${player1.turns}`)
+    checkWin(player1.turns);
+    renderConsole();
+
+
+    turn = parseInt(prompt("Player X Turn"));
+    gameBoard.splice(turn - 1, 1, "X");
+    player1.turns.push(turn);
+    console.log(`player turns ${player1.turns}`)
+    checkWin(player1.turns);
+    renderConsole();
+
+    turn = parseInt(prompt("Player X Turn"));
+    gameBoard.splice(turn - 1, 1, "X");
+    player1.turns.push(turn);
+    console.log(`player turns ${player1.turns}`)
+    checkWin(player1.turns);
+    renderConsole();
+
+
+
+
+
+
+    // if (isGameRunning === false) {
+    //     return;
+    // }
+    // }
+
+    function renderConsole() {
+        console.log(gameBoard[0], gameBoard[1], gameBoard[2])
+        console.log(gameBoard[3], gameBoard[4], gameBoard[5])
+        console.log(gameBoard[6], gameBoard[7], gameBoard[8])
+    }
+
+    // const test1 = [1, 2, 3];
+    // const test2 = [4, 5, 6];
+    // const test3 = [9, 8, 7];
+    // const test4 = [5, 1, 2, 3];
+    // const test5 = [9, 6, 3, 2, 1];
+    // const test11 = [1, 3, 5, 7];
+    // const test6 = [1, 4, 9];
+    // const test7 = [1, 6, 8];
+    // const test8 = [1, 2, 4, 5];
+    // const test9 = [0, 1, 8, 9];
+    // const test10 = [1, 3, 7, 9];
+    // const test12 = [5, 8, 7];
+
+    // console.log(checkWin(test1));
+    // console.log(checkWin(test2));
+    // console.log(checkWin(test3));
+    // console.log(checkWin(test4));
+    // console.log(checkWin(test5));
+    // console.log(checkWin(test11));
+    // console.log("---------------");
+    // console.log(checkWin(test6));
+    // console.log(checkWin(test7));
+    // console.log(checkWin(test8));
+    // console.log(checkWin(test9));
+    // console.log(checkWin(test10));
+    // console.log(checkWin(test12));
+
+
+})();
+
+
+
+// testing 
+
+/*     const test1 = [1, 2, 3];
+    const test2 = [4, 5, 6];
+    const test3 = [9, 8, 7];
+    const test4 = [5, 1, 2, 3];
+    const test5 = [9, 6, 3, 2, 1];
+    const test11 = [1, 3, 5, 7];
+    const test6 = [1, 4, 9];
+    const test7 = [1, 6, 8];
+    const test8 = [1, 2, 4, 5];
+    const test9 = [0, 1, 8, 9];
+    const test10 = [1, 3, 7, 9];
+    const test12 = [5, 8, 7];
 
     console.log(checkWin(test1));
     console.log(checkWin(test2));
@@ -63,4 +149,7 @@ By Mathew Schaeffer
     console.log(checkWin(test9));
     console.log(checkWin(test10));
     console.log(checkWin(test12));
-})();
+
+
+
+    */
